52d4245835ddee7aca383a1f01a86c0fe291f6ff
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-04 11:19:36 +0530

    ll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Hero = () => {
+  
+  const phoneNumber = "+918083333328"
+
+  const openWhatsApp = ()=>{
+    const whatsappUrl = `https://wa.me/${phoneNumber}`;
+    window.open(whatsappUrl, '_blank');
+  }
+
   return (
     <div className='hero-one'>
       <img src= "bg-two.png"  className='hero-img'/>
+      <div className="whatsapp-icon" onClick={openWhatsApp}>
+        {/* Add your WhatsApp icon image or SVG here */}
+        <img src="wa.png" alt="WhatsApp" />
+      </div>
     </div>
   )
 }
