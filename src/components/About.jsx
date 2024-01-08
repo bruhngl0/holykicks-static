13071fbfd835e438a6523024aec0340e7c4130fd
bruh.ngl0 <adityashrm500@gmail.com>
2024-01-08 15:18:00 +0530

    price

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const About = () => {
   return (
     <div>
@@ -7,15 +8,16 @@ const About = () => {
        <video src= "kick.mp4" className='about-video' controls/>
        <p className='services-text'>BOOK PICKUP</p>
 
-       <a href='https://play.google.com/store/apps/details?id=com.dunzo.user&hl=en_IN&gl=US&pli=1' target='_blank'>
-       <div className='about-container'>
-        <p>
-        Book Dunzo/Swiggy and drop your kicks at: <br></br> <br></br>25/26 BMT Complex, Malleshwaram-Bangalore 560003 
-        </p>
-        <img src= "g4.png" className='sw'/>
-       
-      </div>
-      </a>
+            <a href= "https://play.google.com/store/apps/details?id=com.dunzo.user&hl=en_IN&gl=US&pli=1" target='_blank'>
+            <div className='about-container'>
+                <p>
+                    Book Dunzo/Swiggy and drop your kicks at: <br /><br />
+                    25/26 BMT Complex, Malleshwaram-Bangalore 560003
+                </p>
+                <img src="g4.png" className='sw' alt="Dunzo App" />
+            </div>
+            </a>
+
       <p className='services-text'>Before/After</p>
       
       <div className='about-image-container'>
