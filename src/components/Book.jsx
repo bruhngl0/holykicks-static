2ee03c326a25a45c9f243a45decb4175f7a7eca2
bruh.ngl0 <adityashrm500@gmail.com>
2024-07-16 14:57:01 +0530

    height

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Book = () => {
+  const handleButtonClick = (event) => {
+    event.stopPropagation();
+    // Button specific functionality
+    console.log('Button clicked!');
+  };
+
   return (
-    <button className='book-now'>
-        BOOK NOW      
+    <button className='book-now' onClick={handleButtonClick}>
+      BOOK NOW
     </button>
-  )
-}
-export default React.memo(Book)
- 
+  );
+};
+
+export default React.memo(Book);
